3a5a04e4f63ce568bd7a81cfb5f99479b4cdcf40
yaburin <ryuseiarekkusu110707@yahoo.co.jp>
2026-03-17 14:50:10 +0900

    Update debug.lua

--- .config/nvim/lua/plugins/neovim/debug.lua
+++ .config/nvim/lua/plugins/neovim/debug.lua
@@ -1,22 +1,12 @@
 return {
-
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
-			{ "theHamsta/nvim-dap-virtual-text", opts = {
-				virt_text_pos = "eol",
-			} },
+			{ "theHamsta/nvim-dap-virtual-text", opts = { virt_text_pos = "eol" } },
 			{ "jay-babu/mason-nvim-dap.nvim", opts = {} },
 			"mfussenegger/nvim-dap-python",
-			-- {
-			-- 	"mrcjkb/rustaceanvim",
-			-- 	version = "^8", -- Recommended
-			-- 	ft = { "rust" },
-			-- 	event = { "BufReadPost", "BufNewFile" }, --*
-			-- },
-			-- { "dchinmay2/clangd_extensions.nvim", opts = {} },
 		},
 		keys = {
 			{
@@ -42,7 +32,7 @@ return {
 			},
 			{
 				mode = { "n" },
-				"<F11>",
+				"<F12>",
 				function()
 					require("dap").step_out()
 				end,
@@ -56,13 +46,25 @@ return {
 			},
 		},
 		config = function()
+			local orig_notify = vim.notify
+			vim.notify = function(msg, level, opts)
+				if type(msg) == "string" and msg:match("Cursor position outside buffer") then
+					return
+				end
+				orig_notify(msg, level, opts)
+			end
+
 			require("dap-python").setup("python")
+
 			local dap = require("dap")
 			local dapui = require("dapui")
+
 			dapui.setup()
+
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
 			end
+
 			dap.listeners.before.event_terminated["dapui_config"] = function()
 				dapui.close()
 			end
@@ -70,50 +72,117 @@ return {
 			dap.listeners.before.event_exited["dapui_config"] = function()
 				dapui.close()
 			end
-			-- C / C++ / Rust 設定
+
+			local mason_path = vim.fn.stdpath("data") .. "/mason"
+
+			-------------------------------------------------
+			-- adapters
+			-------------------------------------------------
+
 			dap.adapters.codelldb = {
 				type = "server",
 				port = "${port}",
 				executable = {
-					command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb",
+					command = mason_path .. "/packages/codelldb/extension/adapter/codelldb",
 					args = { "--port", "${port}" },
 				},
 			}
 
+			-------------------------------------------------
 			-- C++
-			dap.configurations = {
-				cpp = {
-					{
-						name = "Launch file",
-						type = "codelldb",
-						request = "launch",
-						program = function()
-							local file = vim.fn.expand("%:p")
-							local exe = vim.fn.expand("%:p:r")
-							local cmd = "g++ -g -O0 '" .. file .. "' -o '" .. exe .. "'"
-							vim.fn.system(cmd)
-							return exe
+			-------------------------------------------------
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch file",
+					type = "codelldb",
+					request = "launch",
+
+					program = function()
+						vim.cmd("w")
+
+						local file = vim.fn.resolve(vim.fn.expand("%:p"))
+						local exe = vim.fn.expand("%:p:r")
+
+						local result = vim.fn.system({
+							"g++",
+							"-g",
+							"-O0",
+							file,
+							"-o",
+							exe,
+						})
+
+						if vim.v.shell_error ~= 0 then
+							vim.notify("Build failed:\n" .. result, vim.log.levels.ERROR)
+							return nil
+						end
+
+						return exe
+					end,
+
+					cwd = function()
+						return vim.fn.expand("%:p:h")
+					end,
+
+					stopOnEntry = false,
+					runInTerminal = true,
+
+					sourceMap = {
+						["."] = function()
+							return vim.fn.expand("%:p:h")
 						end,
-						cwd = "${workspaceFolder}",
-						stopOnEntry = false,
-						runInTerminal = true,
 					},
+
+					exitCommands = {},
 				},
-				rust = {
-					{
-						name = "Launch",
-						type = "codelldb",
-						request = "launch",
-						program = function()
-							local cargo_toml = vim.fn.getcwd() .. "/Cargo.toml"
-							local name = vim.fn.system("grep '^name' " .. cargo_toml .. " | head -1 | cut -d'\"' -f2")
-							name = name:gsub("%s+", "")
-							return vim.fn.getcwd() .. "/target/debug/" .. name
-						end,
-						cwd = "${workspaceFolder}",
-						stopOnEntry = false,
-						args = {},
-					},
+			}
+
+			-------------------------------------------------
+			-- Rust
+			-------------------------------------------------
+
+			dap.configurations.rust = {
+				{
+					name = "Launch",
+					type = "codelldb",
+					request = "launch",
+
+					program = function()
+						vim.notify("cargo build 中...", vim.log.levels.INFO)
+
+						local result = vim.fn.system({ "cargo", "build" })
+
+						if vim.v.shell_error ~= 0 then
+							vim.notify("cargo build 失敗:\n" .. result, vim.log.levels.ERROR)
+							return nil
+						end
+
+						local cargo_toml = vim.fn.getcwd() .. "/Cargo.toml"
+
+						local name = vim.fn.system({
+							"grep",
+							"^name",
+							cargo_toml,
+						})
+
+						name = name:match('"([^"]+)"') or ""
+						name = name:gsub("%s+", "")
+
+						if name == "" then
+							vim.notify(
+								"Cargo.toml からパッケージ名を取得できませんでした",
+								vim.log.levels.ERROR
+							)
+							return nil
+						end
+
+						return vim.fn.getcwd() .. "/target/debug/" .. name
+					end,
+
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
 				},
 			}
 		end,
